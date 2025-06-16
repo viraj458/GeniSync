849bfb410184336621baa87f6a4980b0a7e2707c
Viraj Malaka <44742789+viraj458@users.noreply.github.com>
2025-06-16 10:54:29 +0530

    Update with google ad tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2907733698174063"
+           crossorigin="anonymous">
+          </script>
+        </head>
         <body className={inter.className}>
           <Providers>{children}</Providers>
         </body>
